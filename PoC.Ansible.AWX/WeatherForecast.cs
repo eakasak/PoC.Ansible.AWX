4b0290de4adf0a7b51edc5788299bf392bb8df81
eakasak piboonkit <eakasak.pib@truedigital.com>
2022-04-09 13:47:55 +0700

    commit

---
+++ PoC.Ansible.AWX/WeatherForecast.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace PoC.Ansible.AWX
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public class VCenter
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public string Testconnect { get; set; }
+    }
+}
+
